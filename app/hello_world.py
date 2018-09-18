cb12571b104ae62f99c3d1c88cdb4939c28cc768
Noah Yoshida <nyoshida@nd.edu>
2018-09-17 20:53:29 -0400

    hello pusheen

--- app/hello_world.py
+++ app/hello_world.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import cv2
 import sys
 
 def hello(file_name):
-    img = cv2.imread(file_name, 0)
+    img = cv2.imread(file_name, 1)
     cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
