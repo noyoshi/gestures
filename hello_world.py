4826c119a0ca0748f454f1a638ddea5f2623a6c9
Noah Yoshida <nyoshida@nd.edu>
2018-09-17 20:51:50 -0400

    hello world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+import sys
+
+def hello(file_name):
+    img = cv2.imread(file_name, 0)
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    hello(file_name)
