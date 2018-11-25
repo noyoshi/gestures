d5552e5d8c1e14ba1b955955b95c872fec20b0f7
Tina Wu <tinawu.nd23@gmail.com>
2018-11-25 15:50:05 -0500

    CART and KNN

---
+++ cart.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+df_training = pd.read_csv("training_data.txt")
+train_data, train_label = df_training.loc[:,df_training.columns != 'label'], df_training['label']
+df_testing = pd.read_csv("testing_data.txt")
+test_data, test_label = df_testing.loc[:,df_testing.columns != 'label'], df_testing['label']
+
+clf = DecisionTreeClassifier()
+y_pred = clf.fit(train_data, train_label)
+predictions = clf.predict(test_data)
+
+print(predictions)
