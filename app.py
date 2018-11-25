322569cb177e706bae4954a5de0c758e2cf5cfc6
Tina Wu <tinawu.nd23@gmail.com>
2018-11-25 00:54:06 -0500

    added vague shaka and rock on recognition

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def removeBG(f, bgModel):
 def convex_hull_classifier(filtered_img, og_img):
     # Find contours
     _,contours,hierarchy= cv2.findContours(filtered_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) == 0:
+        return
 
     # Get the max contour (hand)
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
@@ -68,7 +71,7 @@ def convex_hull_classifier(filtered_img, og_img):
 
         #distance between point and convex hull
         d=(2*ar)/a
-        # print(arearatio, d)
+        print(ar, d)
 
         # apply cosine rule here
         angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
@@ -90,15 +93,24 @@ def convex_hull_classifier(filtered_img, og_img):
         if areacnt<2000:
             cv2.putText(og_img,'Put hand in the box',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
         else:
-            cv2.putText(og_img,'good job!',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            if d<15:
+                cv2.putText(og_img,'good job!',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            else:
+                cv2.putText(og_img,'shaka',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
     elif l==2:
         cv2.putText(og_img,'peace',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
     elif l==3:
-        cv2.putText(og_img,'3',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+        if ar>2000:
+            cv2.putText(og_img,'OK',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+        else:
+            cv2.putText(og_img,'Rock On',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)   
+    '''
     elif l==4:
         cv2.putText(og_img,'4',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
     elif l==5:
         cv2.putText(og_img,'5',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+    ''' 
+    
     elif l==6:
         cv2.putText(og_img,'reposition',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
     else :
